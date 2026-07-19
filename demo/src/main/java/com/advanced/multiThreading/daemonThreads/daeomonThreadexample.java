677761c977d72e1dcf7d062348dca069f36afee8
chaitanya <chaitanyassm@outlook.com>
2026-07-19 22:24:53 +0530

    updated changes

--- demo/src/main/java/com/advanced/multiThreading/daemonThreads/daeomonThreadexample.java
+++ demo/src/main/java/com/advanced/multiThreading/daemonThreads/daeomonThreadexample.java
@@ -1,5 +1,13 @@
 package com.advanced.multiThreading.daemonThreads;
 
 public class daeomonThreadexample {
+
+    public static void main(String[] args) {
+        daemonMyThread t1 = new daemonMyThread();
+        t1.setDaemon(true);
+        t1.start();
+        System.out.println("End of main thread");
+        
+    }
     
 }
