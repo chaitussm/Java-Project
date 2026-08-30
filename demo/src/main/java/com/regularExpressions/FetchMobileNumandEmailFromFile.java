f80211332e4d1c342c5ac4a49e597db4b3ffb989
chaitanya <chaitanyassm@outlook.com>
2026-08-30 14:21:27 +0530

    updated changes

---
+++ demo/src/main/java/com/regularExpressions/FetchMobileNumandEmailFromFile.java
@@ -0,0 +1,13 @@
+package com.regularExpressions;
+
+public class FetchMobileNumandEmailFromFile {
+
+
+    /*
+        This class is used to fetch mobile numbers and email addresses from a file.
+        Here mobile numbers are mixed with normal data
+        We need to fetch the mobiel numbers and emdild from the given file and then 
+        we need to stor in the ouput file 
+    */
+    
+}
